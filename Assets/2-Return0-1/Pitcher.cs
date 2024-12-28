10f04f44ea990aada8795ff2ab28dcf9d284c9f5
germysus <jsuh7227@gmail.com>
2024-12-27 18:47:58 -0600

    Ball throw prototype
    will have to adjust throw speed, may be too difficult considering swing mechanic

---
+++ Assets/2-Return0-1/Pitcher.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using UnityEngine;
+
+namespace Return0
+{
+    public class Pitcher : MonoBehaviour
+    {
+        public GameObject ball;
+        Animator ballAnimator;
+        public float throwTime;
+        public bool ballThrow;
+        float currentTime;
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+            if (this.gameObject.GetComponentInChildren<Animator>())
+            {
+                ballAnimator = this.gameObject.GetComponentInChildren<Animator>();
+                Debug.Log("found");
+            }
+            else
+            {
+                Debug.Log("not found");
+            }
+            throwTime = Random.Range(0.5f, 2.5f);
+            ball.SetActive(false);
+            ballThrow = false;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            currentTime += Time.deltaTime;
+            if (currentTime >= throwTime) ballThrow = true;
+            ThrowBall();
+        }
+
+        void ThrowBall()
+        {
+            if (ballThrow)
+            {
+                Debug.Log("Throw!");
+                ball.SetActive(true);
+                if (ballAnimator) ballAnimator.SetBool("Throw", true);
+            }
+        }
+    }
+}
